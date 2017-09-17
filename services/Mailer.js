e1ed8e12ade4470074dfc891a5f735c768711aac
basimsahaf <bssahaf@uwaterloo.ca>
2017-09-16 20:11:38 -0400

    added mailer function

---
+++ services/Mailer.js
@@ -0,0 +1,44 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+
+class Mailer extends helper.Mail {
+    constructor ({subject,recipient}, content) {
+        super();
+        
+        this.sgApi = sendgrid(keys.sendGridKey);
+        this.from_email = new helper.Email('no-reply@watchtowr.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipient = new helper.Email(recipient);
+        
+        this.addContent(this.body);
+        const personalize = new helper.Personalization();
+        personalize.addTo(recipient);
+        this.addPersonalization(personalize);
+        this.addClickTracking();
+        
+    }
+    
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true,true);
+        
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+    
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+        
+        const response = this.sgApi.API(request);
+        return response;
+    }
+}
+
+module.exports = Mailer;
---
+++ services/templates/templates.js
@@ -0,0 +1,4 @@
+//pass some object as an argument 
+module.exports = () => {
+   return '<div>Hello there</div>';
+};
