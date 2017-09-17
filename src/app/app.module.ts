94bc11840fecbe3fc1a7606e1ea86fd097497f5b
riri279 <jaritachan37@hotmail.com>
2017-09-17 05:22:28 -0400

    no idea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyComponent2Component } from './project/my-component2/my-component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent2Component
   ],
   imports: [
     BrowserModule
